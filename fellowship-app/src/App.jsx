81da10f37a83af880232b3a83a27639f41ecb417
DaraghWallace <daragh.wallace@elastik.com>
2025-03-07 14:08:00 +0800

    Auth added

--- fellowship-app/src/App.jsx
+++ fellowship-app/src/App.jsx
@@ -1,16 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './CSS/App.css'
 
-function App() {
+import { useAuth } from "react-oidc-context";
+
+export default function App() {
+  const auth = useAuth();
   const [count, setCount] = useState(0)
+
+  const signOutRedirect = () => {
+    const clientId = "18q8itbgqrf1aa0itru44co6d6";
+    const logoutUri = "<logout uri>";
+    const cognitoDomain = "https://ap-southeast-2xlqezwsng.auth.ap-southeast-2.amazoncognito.com";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+        <button onClick={() => auth.removeUser()}>Sign out</button>
+      </div>
+    );
+  }
 
   return (
     <div>
-      Hello
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
     </div>
-  )
+  );
+
 }
 
-export default App
--- fellowship-app/src/main.jsx
+++ fellowship-app/src/main.jsx
@@ -1,10 +1,24 @@
+import React from "react";
 import { StrictMode } from 'react'
+import ReactDOM from "react-dom/client";
 import { createRoot } from 'react-dom/client'
 import './CSS/index.css'
 import App from './App.jsx'
 
+import { AuthProvider } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_XlQeZwsNg",
+  client_id: "18q8itbgqrf1aa0itru44co6d6",
+  redirect_uri: "https://main.dsj2oadeoypam.amplifyapp.com/",
+  response_type: "code",
+  scope: "email openid phone",
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
