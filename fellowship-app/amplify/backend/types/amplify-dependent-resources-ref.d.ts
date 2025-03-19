e00aa1dcfe4aea8a7dd1b6b4ae59507a5ce2b6c6
DaraghWallace <daragh.wallace@elastik.com>
2025-03-19 14:45:39 +0800

    back end and config

--- fellowship-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ fellowship-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,37 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "dfellowship": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "dfellowshiptblTriggerb192b2a5": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "fellowshipapp74a77dbc": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "dfellowshiptbl": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}
