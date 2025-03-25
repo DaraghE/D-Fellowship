bb72b53c4cc158ad776f63df620467442c0abb01
DaraghWallace <daragh.wallace@elastik.com>
2025-03-25 09:55:16 +0800

    checking ./aws-exports

--- fellowship-app/src/main.jsx
+++ fellowship-app/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import awsExports from "./aws-exports";
+// import awsExports from "./aws-exports";
 import Amplify from "aws-amplify";
 
 Amplify.configure(awsExports);
