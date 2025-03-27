6449dd50060129f6e1bf2b3ee14def525e66d469
DaraghWallace <daragh.wallace@elastik.com>
2025-03-27 10:48:14 +0800

    auth fix

--- fellowship-app/src/App.jsx
+++ fellowship-app/src/App.jsx
@@ -7,8 +7,6 @@ import { useState } from 'react'
 
 import './CSS/App.css'
 
-import ToDo from './ToDo';
-
 import { useAuth } from "react-oidc-context";
 
 import { UserIn, Waiting } from './Content';
@@ -16,9 +14,14 @@ import FetchData  from './Functions/FetchData';
 
 export default function App() {
   const auth = useAuth();
-
-
-
+  
+  const signOutRedirect = () => {
+    const clientId = "5q3bmulc63f80neg1ecfse6hgp";
+    const logoutUri = "<logout uri>";
+    const cognitoDomain = "https://<user pool domain>";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+  
   if (auth.isLoading) {
     return <div>Loading...</div>;
   }
@@ -30,19 +33,31 @@ export default function App() {
   if (auth.isAuthenticated) {
     console.log("User");
     console.log(auth.user);
+  
+  
+      <div>
+        {content(auth)}
+      </div>  
   }  
+  
+  
   return (
     <>
       <FetchData/>
 
-      <div>
-        {content(auth)}
-      </div>
+
 
       <br/>
       
       <div>
-        {auth_controls(auth)}
+        <div className='sign_in_out'>
+          { 
+            auth.isAuthenticated ? 
+              <button onClick={() => signOutRedirect()}>Sign out</button> 
+              : 
+              <button onClick={() => auth.signinRedirect()}>Sign in</button>
+          }
+        </div> 
       </div>
     </>
   );  
@@ -73,22 +88,3 @@ function content(auth) {
 
 }
 
-const signOutRedirect = () => {
-  const clientId = "5q3bmulc63f80neg1ecfse6hgp";
-  const logoutUri = "<logout uri>";
-  const cognitoDomain = "https://<user pool domain>";
-  window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
-};
-
-function auth_controls(auth) {
-  return (
-    <div className='sign_in_out'>
-      { 
-        auth.isAuthenticated ? 
-          <button onClick={() => signOutRedirect()}>Sign out</button> 
-          : 
-          <button onClick={() => auth.signinRedirect()}>Sign in</button>
-      }
-    </div> 
-  )  
-}
