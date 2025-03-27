1f5ee1989a3a77f22426d18aa3b6891200cc2604
DaraghWallace <daragh.wallace@elastik.com>
2025-03-27 10:36:52 +0800

    deploy test

--- fellowship-app/src/App.jsx
+++ fellowship-app/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 
-import "./amplifyconfiguration";
-import { Amplify } from "aws-amplify";
-import * as awsExports from "./aws-exports.js";
-Amplify.configure(awsExports.default);
+// import "./amplifyconfiguration";
+// import { Amplify } from "aws-amplify";
+// import * as awsExports from "./aws-exports.js";
+// Amplify.configure(awsExports.default);
 
 import './CSS/App.css'
 
